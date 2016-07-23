2bcb6ea441c07a8ab63eb0270fd9c7350823ceb8
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-23 09:07:38 -0600

    Have got the hike json parsed

---
+++ DataModelsAndJsonForProject/DataModelsAndJsonForProject/Hike.swift
@@ -0,0 +1,33 @@
+//
+//  Hike.swift
+//  DataModelsAndJsonForProject
+//
+//  Created by Taylor Frost on 7/23/16.
+//  Copyright © 2016 Taylor Frost. All rights reserved.
+//
+
+import Foundation
+
+class Hike {
+    
+    var hike_name:String = ""
+    var picture:String = ""
+    var difficulty:Int = 0
+    var location:String = ""
+    var info:String = ""
+    var milage:String = ""
+    var elevation_change:String = ""
+    
+    init() {
+        
+        self.hike_name = ""
+        self.picture = ""
+        self.difficulty = 0
+        self.location = ""
+        self.info = ""
+        self.milage = ""
+        self.elevation_change = ""
+       
+    }
+
+}
