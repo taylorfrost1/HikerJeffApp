23ec988b41672635b58da18b6d82e9fccd487eff
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-25 22:06:22 -0600

    Got a practice app sending emails from app

---
+++ UIMessageTestApp/UIMessageTestApp/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  UIMessageTestApp
+//
+//  Created by Taylor Frost on 7/25/16.
+//  Copyright © 2016 Taylor Frost. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    
+    @IBOutlet weak var body: UITextView!
+    @IBOutlet weak var subject: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func sendEmail(sender: UIButton) {
+       
+        var subjectText = "This is Jared"
+        subjectText += subject.text!
+        
+        let messageBody = body
+        
+        let toRecipients = ["taylorhamblinfrost@gmail.com"]
+        
+        let mc: MFMailComposeViewController = MFMailComposeViewController()
+        
+        mc.mailComposeDelegate = self
+        mc.setSubject(subjectText)
+        mc.setMessageBody(messageBody.text, isHTML: false)
+        mc.setToRecipients(toRecipients)
+        
+        self.presentViewController(mc, animated: true, completion: nil)
+  
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        switch result{
+            
+//        case MFMailComposeResultCancelled:
+//            print("Mail Cancelled")
+//        case MFMailComposeResultSaved:
+//            print("Mail Saved")
+////        case is MFMailComposeResult:
+////            print("Mail Sent")
+//        case MFMailComposeResultFailed:
+//            print("Mail Sent Failure: %@", [error?.localizedDescription])
+   
+        default:
+            break
+            
+        }
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
